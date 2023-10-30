92fce7c08d3f6a9c9be4bfbf88c642b9dc5f0b8e
Tanish32 <110172756+Tanish32@users.noreply.github.com>
2023-10-30 10:04:26 +0530

    Update ERC20Token.sol

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -7,21 +7,21 @@ contract ERC20Token {
     uint8 public decimals = 18;
     uint256 public totalSupply = 1000000 * 10 ** uint256(decimals);
     mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
+    // mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    // event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor() {
         balanceOf[msg.sender] = totalSupply;
     }
 
     //allow only if spender is msg.sender
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
+    // function approve(address _spender, uint256 _value) public returns (bool success) {
+    //     allowance[_spender][_spender] = _value;
+    //     emit Approval(_spender, _spender, _value);
+    //     return true;
+    // }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
@@ -33,10 +33,10 @@ contract ERC20Token {
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[_from] >= _value);
-        require(allowance[_from][msg.sender] >= _value);
+        // require(allowance[_from][msg.sender] >= _value);
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
-        allowance[_from][msg.sender] -= _value;
+        // allowance[_from][msg.sender] -= _value;
         emit Transfer(_from, _to, _value);
         return true;
     }
