ceefb1811ae2161473b2ba4c28df298b3cda244b
Tanish32 <110172756+Tanish32@users.noreply.github.com>
2023-10-30 22:55:34 +0530

    Update BusinessLogic.sol

--- BusinessLogic.sol
+++ BusinessLogic.sol
@@ -9,6 +9,7 @@ contract BusinessLogic {
     address public unspentAddress  = 0xdD870fA1b7C4700F2BD7f44238821C26f7392148;  // Burn or store address
     mapping(address => bool) internal purchaseValidated;
     mapping(address => uint256) public discountRate;  // Maps customer address to discount rate
+    mapping(address => bool)    public reg;
 
     event TokensAwarded(address indexed customer, uint256 amount);
     event DiscountUsed(address indexed customer, uint256 discount);
@@ -27,7 +28,7 @@ contract BusinessLogic {
     // Owner validates the purchase
     function validatePurchase(address customer, uint256 amount) internal {
         // require(!purchaseValidated[customer], "Purchase already validated");
-        if(purchaseValidated[customer] = true){
+        if(purchaseValidated[customer] == true){
             require(tokenContract.transferFrom(owner, customer, amount), "Token transfer failed");
             emit TokensAwarded(customer, amount);
             purchaseValidated[customer] = false;
@@ -37,8 +38,7 @@ contract BusinessLogic {
 
     // To be called when a customer purchases a product
  function purchaseProduct() external payable {
-        // require(purchaseValidated[msg.sender], "Purchase not validated");
-
+        require(reg[msg.sender]==true,"Caller not registered");
         uint256 originalPrice = 1 ether;  // Assuming 1 product costs 1 Ether
         uint256 discount = discountRate[msg.sender];
         uint256 finalPrice = originalPrice * (100 - discount) / 100;
@@ -59,8 +59,13 @@ contract BusinessLogic {
         discountRate[msg.sender] = 0;
     }
 
+    function register() external {
+        reg[msg.sender] = true;
+    }
+
     // To be called when a customer wants to use tokens for a discount . i.e to be called before purcaseProduct
     function useDiscount() external {
+        require(reg[msg.sender]==true,"Caller not registered");
         uint256 tokenCount = tokenContract.balanceOf(msg.sender);
         uint256 discount = 0;
 
