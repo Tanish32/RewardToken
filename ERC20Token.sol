bf9b1a3623189df2250bd733f0d50e2971ca5561
Tanish32 <110172756+Tanish32@users.noreply.github.com>
2023-10-31 21:38:03 +0530

    Update ERC20Token.sol

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -2,41 +2,37 @@
 pragma solidity ^0.8.0;
 
 contract ERC20Token {
-    string public name = "XYZToken";
-    string public symbol = "XYZ";
+    string public name = "RewardToken";
+    string public symbol = "RTK";
     uint8 public decimals = 18;
     uint256 public totalSupply = 1000000 * 10 ** uint256(decimals);
+    address public owner;
+    address businessLogicContract;
     mapping(address => uint256) public balanceOf;
-    // mapping(address => mapping(address => uint256)) public allowance;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
-    // event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor() {
         balanceOf[msg.sender] = totalSupply;
+        owner = msg.sender;
     }
 
-    //allow only if spender is msg.sender
-    // function approve(address _spender, uint256 _value) public returns (bool success) {
-    //     allowance[_spender][_spender] = _value;
-    //     emit Approval(_spender, _spender, _value);
-    //     return true;
-    // }
-
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
-        return true;
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
     }
 
+    function businessLogicAddress(address _businessLogicContract) external onlyOwner{
+        businessLogicContract = _businessLogicContract;
+    }
+    
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require( _from == msg.sender || msg.sender == businessLogicContract,"Cannot use another person's funds");
         require(balanceOf[_from] >= _value);
-        // require(allowance[_from][msg.sender] >= _value);
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
-        // allowance[_from][msg.sender] -= _value;
         emit Transfer(_from, _to, _value);
         return true;
     }
