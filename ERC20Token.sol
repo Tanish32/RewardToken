c23e4128b137003e3251f5f454ac827fe3e51051
Tanish32 <110172756+Tanish32@users.noreply.github.com>
2023-10-31 21:55:16 +0530

    Update ERC20Token.sol

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -9,7 +9,6 @@ contract ERC20Token {
     address public owner;
     address businessLogicContract;
     mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
